96d06e9ab0490747ee1d4b89a66ed4eec76824b7
pamu47 <pamudithamaduranga@gmail.com>
2019-06-23 15:58:07 +0530

    all routes added

--- server/server.js
+++ server/server.js
@@ -11,13 +11,25 @@ app.use(bodyParser.urlencoded({
     extended : false
 }))
 
-const mongoURI = 'mongodb://localhost:27017/ticketingsystem'
+const mongoURI = 'mongodb://localhost:27017/SImgt'
 
 mongoose.connect(mongoURI, {useNewUrlParser : true})
         .then(()=> console.log("Database connection success"))
         .catch(err => console.log("Error while connecting to Database " +err))
 
 //Add your routes here
+const course = require('./routes/Instructor/course.routes')
+const assignment = require('./routes/Instructor/assignment.routes')
+const submission = require('./routes/Instructor/submissions.routes')
+const exam = require('./routes/Instructor/exam.routes')
+const examSubmission = require('./routes/Instructor/exam-submissions.routes')
+
+app.use('/', course)
+app.use('/', assignment)
+app.use('/',submission)
+app.use('/', exam)
+app.use('/',examSubmission)
+
 
 
 app.listen(port, () => {
--- webclient/src/App.js
+++ webclient/src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter as Router, Route, Link} from "react-router-dom"
+
 import Dashbord from './components/Instructor/Instructor-Dashboard'
+import ChooseCourse from './components/Instructor/chooseCourses'
+import GradeAssignments from './components/Instructor/gradeAssignments'
+import Exam from './components/Instructor/exam'
+import Assignment from './components/Instructor/assignment'
+import Result from './components/Instructor/results'
 
 class App extends Component {
   render() {
@@ -9,10 +15,15 @@ class App extends Component {
       <Router>
         <div>
           <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
-            <a class="navbar-brand mr-1" href="home.php">Instructor Portal</a>
+            <Link to='/' class="navbar-brand mr-1">Instructor Portal</Link>
           </nav>
         </div>
         <Route exact path='/' component={Dashbord} />
+        <Route path='/choose-course' component={ChooseCourse} />
+        <Route path='/grade-assignments' component={GradeAssignments} />
+        <Route path='/exam/:handle' component={Exam} />
+        <Route path='/assignment/:handle' component={Assignment} />
+        <Route path='/result/:handle' component={Result} />
       </Router>
     );
   }
